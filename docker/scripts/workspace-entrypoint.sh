01649a1dd58cac96f0a0865934e527c73a3fbbb3
vladmosoiu98 <vlad.m@agorarobotics.com>
2023-10-26 17:44:24 +0300

    remove domain id from entrypoint script

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -12,8 +12,8 @@
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
-echo "export ROS_DOMAIN_ID=3" >> ~/.bashrc
-export ROS_DOMAIN_ID=3
+#echo "export ROS_DOMAIN_ID=3" >> ~/.bashrc
+#export ROS_DOMAIN_ID=3
 
 sudo apt-get update
 rosdep update
@@ -24,4 +24,5 @@ sudo service udev restart
 rosdep install --from-paths src -y --ignore-src
 
 source install/setup.bash
+
 $@
